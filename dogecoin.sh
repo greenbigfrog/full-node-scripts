6b2edc2f11b64eeacaf9f0d01c325febb1b619b0
Jonathan B <greenbigfrog@gmail.com>
2017-12-26 05:09:13 +0100

    add dogecoin.sh

---
+++ dogecoin.sh
@@ -0,0 +1,14 @@
+su root
+apt update
+apt -y upgrade
+apt -y install htop ufw
+wget https://github.com/dogecoin/dogecoin/releases/download/v1.10.0/dogecoin-1.10.0-linux64.tar.gz
+tar -zxvf dogecoin-1.10.0-linux64.tar.gz
+mkdir ~/.dogecoin
+rpcuser=sgdsdg >> ~/.dogecoin/dogecoin.conf
+rpcpassword=dgskgdsk >> ~/.dogecoin/dogecoin.conf
+wget -O ~/.dogecoin/bootstrap.dat https://bootstrap.chain.so/bootstrap.dat
+~/dogecoin-1.10.0/bin/dogecoind &
+ufw allow 22
+ufw allow 22556
+ufw --force enable
