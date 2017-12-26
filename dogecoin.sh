28b76531fd82e149e1c651b285054abe2dc33650
Jonathan B <greenbigfrog@gmail.com>
2017-12-26 13:52:51 +0100

    add swap setup

--- dogecoin.sh
+++ dogecoin.sh
@@ -9,6 +9,14 @@ echo 'rpcuser=sgdsdg' >> ~/.dogecoin/dogecoin.conf
 echo 'rpcpassword=dgskgdsk' >> ~/.dogecoin/dogecoin.conf
 wget -O ~/.dogecoin/bootstrap.dat https://bootstrap.chain.so/bootstrap.dat
 ~/dogecoin-1.10.0/bin/dogecoind &
+echo 'setting up firewall'
 ufw allow 22
 ufw allow 22556
 ufw --force enable
+echo 'enabling swap'
+fallocate -l 4G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' >> /etc/fstab
