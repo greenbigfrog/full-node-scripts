bc79a09a0cfef78f023e06584633bce7e5d46e8c
Jonathan B <greenbigfrog@gmail.com>
2017-12-26 05:34:18 +0100

    fix echo

--- dogecoin.sh
+++ dogecoin.sh
@@ -5,8 +5,8 @@ apt -y install htop ufw
 wget https://github.com/dogecoin/dogecoin/releases/download/v1.10.0/dogecoin-1.10.0-linux64.tar.gz
 tar -zxvf dogecoin-1.10.0-linux64.tar.gz
 mkdir ~/.dogecoin
-rpcuser=sgdsdg >> ~/.dogecoin/dogecoin.conf
-rpcpassword=dgskgdsk >> ~/.dogecoin/dogecoin.conf
+echo 'rpcuser=sgdsdg' >> ~/.dogecoin/dogecoin.conf
+echo 'rpcpassword=dgskgdsk' >> ~/.dogecoin/dogecoin.conf
 wget -O ~/.dogecoin/bootstrap.dat https://bootstrap.chain.so/bootstrap.dat
 ~/dogecoin-1.10.0/bin/dogecoind &
 ufw allow 22
